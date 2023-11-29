07ad9bab59fa2a9a446f1d7545457484a5f4b064
Your Name <you@example.com>
2023-11-29 11:25:36 +0700

    create product store

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,27 @@
+import getBillboards from "@/actions/get-billboard";
+import Container from "@/components/ui/container";
+import Billboard from "@/components/billbaord";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+    const products = await getProducts( { isFeatured: true });
+    const billboard = await getBillboards("1300d9b4-09e7-4d64-8e35-60f913aa5a54");
+    return (
+        <Container>
+            <div className="space-y-10 pb-10">
+                <Billboard data={billboard} />
+            </div>
+            <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                <ProductList 
+                    title="Featured Products"
+                    items={products}
+                />
+            </div>
+        </Container>
+    )
+}
+
+export default HomePage;
